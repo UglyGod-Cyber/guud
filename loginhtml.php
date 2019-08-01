355bb791841a136e73274801b4e4f8473045c4e6
UglyGod-Cyber <52437450+UglyGod-Cyber@users.noreply.github.com>
2019-08-01 11:40:27 +0800

    Add files via upload

---
+++ loginhtml.php
@@ -0,0 +1,8 @@
+<html>
+<form action=login.php method=POST>
+Username:<input type=text name=uname size=25><br>
+Password:<input type=text name=pwd size=25><br>
+<input type="submit" value="Submit your username">
+
+</form>
+</html>
